f0f8877c1c44e6f74ff7448a7c3183547ca66204
li195724763 <leeleehomework@gmail.com>
2021-04-05 00:39:02 -0400

    multiple exceptions

--- OCP/src/Chapter6/Chapter6.java
+++ OCP/src/Chapter6/Chapter6.java
@@ -4,7 +4,19 @@ public class Chapter6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try{
+			method_1();
+		}catch(ArrayIndexOutOfBoundsException | RuntimeException e) {//DOES NOT COMPILE : catch(ArrayIndexOutOfBoundsException | RuntimeException e)
+			
+		}
 
+	}
+	
+	public static void method_1() {
+		
 	}
 
 }
+
+
