01fb36fe8b74b90e19e17bd2394b7dd324bd1a9a
li195724763 <leeleehomework@gmail.com>
2021-04-20 22:51:02 -0400

    April 20 2021

--- OCP/src/Chapter7/Chapter7.java
+++ OCP/src/Chapter7/Chapter7.java
@@ -1,5 +1,10 @@
 package Chapter7;
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Chapter7 {
 	
@@ -16,10 +21,24 @@ public class Chapter7 {
 			System.out.println("ReadInventoryThread extends Thread");		
 		}
 	}
+	
+	public class SheepManager{
+		private AtomicInteger count = new AtomicInteger(0); // need constructor
+		
+		//private Object lock = new Object();
+		private void incrementCount() {
+			//lock = new ArrayList<String>();
+			synchronized (SheepManager.class) {
+				System.out.println("count is : " + count.incrementAndGet());
+			}
+		}
+		
+	}
 	private static int counter = 0;
 	public static final Chapter7 OBJECT = new Chapter7();
 	static ExecutorService staticExecutor = Executors.newSingleThreadExecutor();
 	static int UTILITY_INT = 0;
+	static int _count = 0;
 	public static void main(String[] args) throws InterruptedException, InterruptedException, TimeoutException , ExecutionException {
 		/*DOES NOT COMPILE
 		 * Runnable tc2 = () -> 2;//DOES NOT COMPILE
@@ -63,6 +82,17 @@ public class Chapter7 {
 		OBJECT.method_3();
 		OBJECT.executorServiceUsingCallable();
 		schedulerTest();
+		System.out.println("available process is : " + Runtime.getRuntime().availableProcessors());
+		OBJECT.synchronizedTest();
+		reproduceConcurrentException();
+		reproduceConcurrentExceptionOnList();
+		concurrentCollection();
+		testBlockingQueues();
+		testCopyOnWrite();
+		testObtainingSynchronizedCollections();
+		testParallelStream();
+		testParallelReduction();
+		testParallelCollector();
 		
 	}
 	
@@ -154,13 +184,176 @@ public class Chapter7 {
 		service.schedule(call, 1, TimeUnit.SECONDS);
 		service.schedule(run, 1, TimeUnit.SECONDS);
 		
-		service.scheduleAtFixedRate(() -> System.out.println("I love you April"), 1, 3,  TimeUnit.SECONDS);
-		
-		service.scheduleWithFixedDelay(() -> System.out.println("MUAAAA"), 1, 5, TimeUnit.SECONDS);
-		
-		
+		service.scheduleAtFixedRate(() -> System.out.println("I love you April"), 1, 3,  TimeUnit.SECONDS);		
+		service.scheduleWithFixedDelay(() -> System.out.println("MUAAAA"), 1, 5, TimeUnit.SECONDS);	
 		service.scheduleWithFixedDelay(() -> System.out.println("尿妈妈， 尿妈妈， 尿妈妈！！！！"), 1, 10, TimeUnit.SECONDS);
+		service.shutdown();
+	}
+	
+	public void synchronizedTest() throws InterruptedException {
+		ExecutorService service = Executors.newFixedThreadPool(20);
+		SheepManager manager = new SheepManager();
+		for(int i=0;i<10;i++) {
+			synchronized (manager) {
+				service.submit(() -> manager.incrementCount());
+			}
+			//Thread.sleep(100);
+		}	
+			
+		service.shutdown();
+	}
+	
+	private static void staticSynchronized() {
+		//final Object lock = new Object();
+		synchronized (Chapter7.class){
+			System.out.println("UTILITY_INT is : " + ++UTILITY_INT);
+		} 
 		
+	}
+	
+	private static void reproduceConcurrentException() {
+		Map<String,String> map = new HashMap<>();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		map.put("key3", "value3");
+		
+		for(String key : map.keySet()) {
+			//map.remove(key); //java.util.ConcurrentModificationException
+		}
+	}
+	
+	private static void reproduceConcurrentExceptionOnList() {
+		List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3)); 
+		synchronized (Chapter7.class) {
+			for(int i : list) {
+				//list.add(1); //un-synced iterator, even under synchronized block
+			}
+				
+		}
+
+	}
+	
+	private static void concurrentCollection() {
+		Map<String, String> tc1 = new ConcurrentHashMap<>();
+		tc1.put("key1", "value1");
+		tc1.put("key2", "value2");
+		tc1.put("key3", "value3");
+		System.out.println("Concurrent Keys: " + tc1.keySet());
+		System.out.println("Concurrent Values: " + tc1.values());
+		
+		Queue<Integer> tc2 = new ConcurrentLinkedQueue<>();
+		tc2.offer(1);
+
+		System.out.println("Concurrent Linked Queue: calling peek: " + tc2.peek());
+		System.out.println("Concurrent Linked Queue: calling poll: " + tc2.poll());
+		
+		Deque<Integer> tc3 = new ConcurrentLinkedDeque<>();
+		tc3.push(2);
+		tc3.offer(3);
+		System.out.println("Concurrent Linked Dequeue, double-ended, calling peek: " + tc3.peek());
+		System.out.println("Concurrent Linked Dequeue, double-ended, calling pop: " + tc3.pop());
+		
+		
+	}
+	
+	private static void testBlockingQueues() {
+
+		BlockingQueue<Integer> tc1 = new LinkedBlockingQueue<>();
+		BlockingDeque<Integer> tc2 = new LinkedBlockingDeque<>();
+		try {
+			tc1.offer(1, 10, TimeUnit.MILLISECONDS);
+			System.out.println("Testing LinkedBlockingQueue with calling peek: " + tc1.peek());
+			System.out.println("Testing LinkedBlockingQueue with calling poll: " + tc1.poll(10, TimeUnit.MILLISECONDS));
+			
+			tc2.offer(1);
+			tc2.offerFirst(2, 1, TimeUnit.MICROSECONDS);
+			tc2.offerLast(3); //213
+			tc2.offer(4);//2134
+			tc2.push(5);//52134   Front ---- Back
+			System.out.println("BlockingDeque offer method test : " + tc2);
+			
+			System.out.println("BlockingDeque peek method test : " + tc2.peek()); //5
+			System.out.println("BlockingDeque pollFirst method test : " + tc2.pollFirst(1,TimeUnit.MILLISECONDS)); //5
+			System.out.println("BlockingDeque now is : " + tc2); //2134
+			System.out.println("BlockingDeque pollLast test : " + tc2.pollLast(1, TimeUnit.MICROSECONDS)); //4
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	private static void testCopyOnWrite( ) {
+		List<Integer> tc1 = new CopyOnWriteArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+		for(int i : tc1) {
+			tc1.add(i+1);
+			System.out.println("value is + " + i + " CopyOnWriteArrayList size after change : " + tc1.size());
+		}
+		
+		System.out.println("CopyOnWriteArrayList test: " + tc1);
+	}
+	
+	private static void testObtainingSynchronizedCollections() {
+		List<Integer> tc1 = Collections.synchronizedList(Arrays.asList(1,2,3));
+		//synchronized (tc1) {
+			for(int i : tc1) {
+				System.out.println("testObtainingSynchronizedCollections List : " + i);
+				//tc1.remove(0);// throw java.lang.UnsupportedOperationException
+			}
+		//}	
+			
+		Map<String, String> tc2 = Collections.synchronizedMap(new HashMap<>());
+		tc2.put("key1", "value1");
+		tc2.put("key2", "value2");
+		tc2.put("key3", "value3");
+		synchronized (tc2) {
+			for(String i : tc2.keySet()) {
+				System.out.println("testObtainingSynchronizedCollections Map: " + tc2.get(i));
+				//tc2.remove(i);//throw java.util.ConcurrentModificationException
+			}
+		}
+	}
+	
+	public static void testParallelStream() {
+		Stream<Integer> tc1 = Arrays.asList(1,2,3,4,6).parallelStream();
+		//tc1.forEach(System.out::println);
+		//tc1.forEachOrdered(System.out::println);
+		
+		List<Integer> tc2 = new ArrayList<>();// if both thread trigger the resize of ArrayList at the same time, one result could be lost. 
+		System.out.print("testParallelStream: ");
+		tc1.map(a -> {tc2.add(a); return a;}).forEachOrdered(System.out::print);	
+		System.out.println(" " + tc2);
+	}
+	
+	public static void testParallelReduction() {
+		System.out.println(Arrays.asList(1,2,3,4,5).stream().findAny());
+		System.out.println(Arrays.asList(1,2,3,4,5).parallelStream().findAny()); //result is not predicatable.
+		
+		Stream<Character> tc1 = Stream.of('w', 'o', 'l', 'f').parallel(); 
+		System.out.println(tc1.reduce("" , (c , s1) -> c+s1, (s2,s3) -> s2 + s3)); //follow rule number 1: identity is not affecting the result
+		
+		Stream<Character> tc2 = Stream.of('w', 'o', 'l', 'f').parallel(); 
+		System.out.println(tc2.reduce("X" , (c , s1) -> c+s1, (s2,s3) -> s2 + s3)); //breaking rule number 1 , E.g. : XwXoXlXf
+		
+		Stream<Integer> tc3 = Stream.of(1,2,3,4,5,6,7,8,9,10).parallel();
+		System.out.println(tc3.reduce(0, (num1, num2) -> num1+num2));// follow the second rule: the operator is associate.
+		
+		Stream<Integer> tc4 = Stream.of(1,2,3,4,5,6).parallel();
+		System.out.println(tc4.reduce(0, (num1, num2) -> num1 - num2));// breaking the second rule
+	}
+	
+	public static void testParallelCollector() {
+		Stream<String> tc1 = Stream.of("w", "o", "l", "f").parallel();
+		System.out.println(tc1.collect(ConcurrentSkipListSet::new, Set::add, Set::addAll));
+		
+		Stream<String> tc2 = Stream.of("lion", "tiger", "bear").parallel();
+		ConcurrentMap<Integer,String> tc3 = tc2.collect(Collectors.toConcurrentMap(s->s.length(), v->v, (s1,s2) -> s1 + ", " + s2));
+		System.out.println(tc3);
+		
+		Stream<String> tc4 = Stream.of("lion", "tiger", "bear").parallel();
+		ConcurrentMap<Integer,List<String>> tc5 = tc4.collect(Collectors.groupingByConcurrent(s -> s.length()));
+		System.out.println(tc5);
 	}
 
 }
---
+++ OCP/src/Chapter7/WhaleCalculator.java
@@ -0,0 +1,36 @@
+package Chapter7;
+import java.util.*;
+
+public class WhaleCalculator {
+	public int processRecord(int input) {
+		try {
+			Thread.sleep(10);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		//System.out.println("executed");
+		return input + 1;
+	}
+	
+	public void processAllData(List<Integer> data) {
+		data.parallelStream().map(a -> processRecord(a)).forEach(System.out::println);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhaleCalculator calculator = new WhaleCalculator();
+		
+		List<Integer> data = new ArrayList<>();
+		for(int i=0;i<4000;i++) data.add(i);
+		
+		long start = System.currentTimeMillis();
+		
+		calculator.processAllData(data);
+		
+		double time = (System.currentTimeMillis()-start)/1000.0;
+		
+		System.out.println("\nTask completed in: " + time + "seconds");
+	}
+
+}
